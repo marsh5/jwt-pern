e4897f6259cd1750717e7e9c5e6e67e2c665de8e
marsh5 <brodiemarshall567@gmail.com>
2021-04-02 18:12:41 -0400

    set up login and registration routes

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
-const pool = require('../db')
+const pool = require('../db');
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator");
+const validInfo = require("../middleware/validinfo");
 
 //registering 
 
-router.post('/register', async (req,res) => {
+router.post('/register', validInfo, async (req,res) => {
     try {
         
         //1. destructure the req.body (name, email, password)
@@ -16,13 +19,68 @@ router.post('/register', async (req,res) => {
             email
         ]);
 
-        res.json(user.rows)
+        if(user.rows.length !== 0){
+            return res.status(401).send('user already exists')
+        }
 
         //3. Bcrypt the user password
 
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+
+        const bcryptPassword = await bcrypt.hash(password, salt)
+
         //4. enter the new user inside our database.
 
-        //5. generating or jwt token
+        const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword]);
+
+        //5. generating our jwt token
+
+        const token = jwtGenerator(newUser.rows[0].user_id);
+
+        res.json({ token });
+
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//login route
+
+router.post('/login', validInfo, async (req, res) => {
+    try {
+        
+        //1. destructure the req.body
+
+        const { email, password } = req.body;
+
+        //2. check if user doesn't exist (if not then we throw error)
+
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1",  [
+            email
+        ]);
+
+        if(user.rows.length === 0){
+            return res.status(401).semd("Password or email is incorrect")
+        }
+
+        //3. check if incoming password is the same as the database password
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+
+        console.log(validPassword);
+
+        if(!validPassword){
+            return res.status(401).semd("Password or email is incorrect")
+        }
+
+        //4. give them the jwttoken
+
+        const token = jwtGenerator(user.rows[0].user_id);
+
+        res.json({ token });
 
     } catch (err) {
         console.error(err.message);
