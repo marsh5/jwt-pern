bb80ba895a529ba9aa673938371555e9b4a7aa8e
marsh5 <brodiemarshall567@gmail.com>
2021-04-14 14:56:41 -0400

    built login and register components

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
-function Register() {
+function Register({ setAuth }) {
 
     const [inputs, setInputs] = useState({
         email: "",
@@ -14,21 +15,46 @@ function Register() {
         setInputs({...inputs, [e.target.name] : e.target.value });
     }
 
+    const onSubmitForm = async (e) => {
+        e.preventDefault()
+        try {
+            const body = { email, password, name}
+
+           const response = await fetch("http://localhost:5000/auth/register", {
+               method: "POST",
+               headers: { "Content-Type": "application/json"},
+               body: JSON.stringify(body)
+           });
+
+           const parseRes = await response.json();
+
+           localStorage.setItem("token", parseRes.token)
+           setAuth(true);
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
 
     return (
         <>
             <h1 className="text-center my-5">Register</h1>
-            <form>
+            <form onSubmit = {onSubmitForm}>
                 <input className="form-control my-3" type="email" name="email" placeholder="email" 
                 value = {email}
                 onChange={onChange}/>
 
-                <input className="form-control my-3" type="password" name="password" placeholder="password" />
+                <input className="form-control my-3" type="password" name="password" placeholder="password" 
+                value = {password}
+                onChange={onChange}/>
 
-                <input className="form-control my-3" type="text" name="text" placeholder="text" />
+                <input className="form-control my-3" type="text" name="name" placeholder="username" 
+                value = {name}
+                onChange={onChange}/>
 
                 <button className="btn btn-success btn-block">Submit</button>
             </form>
+            <Link to='/login'>Login</Link>
         </>
     )
 }
